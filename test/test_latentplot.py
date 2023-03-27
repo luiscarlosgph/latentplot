974db6ce92addeb0dd2daf9e9788c23d0c4ef65b
Luis C. Garcia-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2023-03-27 20:02:01 +0100

    Working on the tests.

--- test/test_latentplot.py
+++ test/test_latentplot.py
@@ -20,62 +20,90 @@ import latentplot
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 
-def get_cifar10_samples(n):
+def get_cifar10_samples(n: int) -> np.ndarray:
     """
     @brief Get a random list of samples from the training set of CIFAR-10.
     @param[in]  n  Number of samples to draw, max is 50K.
-    @returns a list of (image, label) pairs from CIFAR-10. The shape of the
-             images returned is (1, 3, 384, 384). The images are RGB.
+    @returns a list of (image, label) pairs from CIFAR-10. The images returned
+             are in shape (H, W, 3), and they are BGR.
     """
-    """
-    # Prepare CIFAR-10 dataloader
-    preproc_tf = torchvision.transforms.Compose([
-        torchvision.transforms.RandomCrop(32, padding=4),
-        torchvision.transforms.Resize(size=384),
-        torchvision.transforms.ToTensor(),
-    ])
-    dataset = torchvision.datasets.CIFAR10(root=tempfile.gettempdir(), 
-                                            train=True, download=True,
-                                            transform=preproc_tf)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1,
-                                             shuffle=True, num_workers=1)
-    dataiter = iter(dataloader)
-
-    # Get list of random image pairs (image, label) from CIFAR-10,
-    # each image of size torch.Size([1, 3, 384, 384])
-    return [dataiter.next() for _ in range(n)] 
-    """
-
-    import cv2
-    import numpy as np
-    import torchvision.datasets as datasets
-
     # Load the CIFAR-10 dataset
-    cifar10 = datasets.CIFAR10(root='data/', train=True, download=True)
+    cifar10 = torchvision.datasets.CIFAR10(root=tempfile.gettempdir(), 
+        train=True, download=True)
+    
+    # Check that the number of samples requested is available in CIFAR-10
+    assert(n < len(cifar10))
 
     # Loop over the images and convert them to BGR format
-    images_bgr = []
-    for i in range(len(cifar10)):
-        # Load the image
-            image = np.array(cifar10[i][0])
+    samples = []
+    for i in range(n):
+        # Load the RGB image
+        im = np.array(cifar10[i][0])
 
-                # Convert from RGB to BGR
-                    image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Make sure that the image shape is (H, W, 3)
+        assert(im.shape[2] == 3)
 
-                        # Add the converted image to the list
-                            images_bgr.append(image_bgr)
+        # Load the label
+        label = cifar10[i][1]
+
+        # Convert from RGB to BGR
+        im_bgr = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+        
+        # Add the converted image to the list
+        samples.append((im_bgr, label))
+
+    return samples
 
 
+def get_pascalvoc_samples(n):
+    """
+    @brief Get a random list of samples from the training set of 
+           PascalVOC 2012.
+    @param[in]  n  Number of samples to draw, max is TODO
+    @returns a list of (image, label) pairs from PascalVOC 2012. The images
+             returned are in shape (H, W, 3), and they are BGR.
+
+    """
+    # Load the PascalVOC dataset
+    pascalvoc = torchvision.datasets.VOCClassification(
+        root=tempfile.gettempdir(), image_set='train', year='2012',
+        download=True)
+    
+    # Check that the number of samples requested is available in PascalVOC
+    assert(n < len(pascalvoc))
+    
+    # Loop over the images and convert them to BGR format
+    samples = []
+    for i in range(n):
+        # Load the RGB image
+        im = np.array(pascalvoc[i][0])
+
+        # Make sure that the image shape is (H, W, 3)
+        assert(im.shape[2] == 3)
+
+        # Load the label
+        label = pascalvoc[i][1]
+
+        # Convert from RGB to BGR
+        im_bgr = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+        
+        # Add the converted image to the list
+        samples.append((im_bgr, label))
+    
+    return samples
 
 
+def preprocess_InceptionV3():
+    # TODO
+    pass
 
+
+"""
 def split_cifar10_samples(samples):
-    """
-    @brief Convert a list of samples into two lists, one containing the
-           BGR images, and another one containing the labels.
-    @param[in]  samples  List of (image, label) pairs.
-    @returns two lists, BGR images and labels.
-    """
+    #@brief Convert a list of samples into two lists, one containing the
+    #       BGR images, and another one containing the labels.
+    #@param[in]  samples  List of (image, label) pairs.
+    #@returns two lists, BGR images and labels.
 
     # Bring samples to CPU memory
     cpu_images = [x[0][0].cpu().detach().numpy() for x in samples]
@@ -87,6 +115,7 @@ def split_cifar10_samples(samples):
     bgr_images = [x[...,::-1].copy() for x in rgb_images]
 
     return bgr_images, [x.cpu().detach().numpy() for x in samples[1]]
+"""
 
 
 class TestStringMethods(unittest.TestCase):
@@ -97,9 +126,7 @@ class TestStringMethods(unittest.TestCase):
         """
         # Get samples from CIFAR-10 
         samples = get_cifar10_samples(num_images)
-
-        # Convert samples from shape (1, 3, 384, 384) to 
-        images, labels = split_cifar10_samples(samples)
+        images = [x[0] for x in samples]
 
         # Get latent vector for the images using InceptionV3
         model = videosum.InceptionFeatureExtractor('vector')
@@ -113,9 +140,7 @@ class TestStringMethods(unittest.TestCase):
     def test_tsne_plot(self, num_images=5000):
         # Get samples from CIFAR-10 
         samples = get_cifar10_samples(num_images)
-
-        # Convert samples from shape (1, 3, 384, 384) to 
-        images, labels = split_cifar10_samples(samples)
+        images = [x[0] for x in samples]
 
         # Get latent vector for the images using InceptionV3
         model = videosum.InceptionFeatureExtractor('vector')
